6bb1405f860005fa5003aa5a6a0e9e9a158ce3ca
Franciele B. de Oliveira <32960110+fran-olv@users.noreply.github.com>
2023-07-09 15:33:59 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template, request, json
+import socket
 
 app = Flask(__name__)
 
@@ -8,10 +9,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/vagas')
 def vagas():
-
-    json_file = os.path.join(os.path.dirname(__file__),  'jobs.json')
+    json_file = os.path.join(os.path.dirname(__file__), 'webscrapping/vagas.json')
 
     # Lê o arquivo JSON
     with open(json_file, 'r') as file:
@@ -20,6 +21,13 @@ def vagas():
     # Retorne a página HTML renderizada com as informações do JSON
     return render_template('vagas.html', data=data)
 
+#export FLASK_APP=index.py
+
+
 
 if __name__ == '__ main__':
-    app.run()
+
+    #ip_publico = requests.get('https://api.ipify.org/').text
+    #print(f'IP Publico: {ip_publico}')
+    #host='10.1.124.206'
+    app.run(host='0.0.0.0')
